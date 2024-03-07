8cdf491199c7314eca7d45851b6f514d431fb46c
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2024-03-08 00:59:16 +0530

    added : createSphere to the GUI

--- src/script.js
+++ src/script.js
@@ -7,6 +7,19 @@ import * as CANNON from "cannon-es";
  * Debug
  */
 const gui = new dat.GUI();
+const debugObject = {};
+debugObject.createSphere = () => {
+  //   console.log("create sphere");
+  //createSphere(0.5, { x: 0, y: 3, z: 0 });
+
+  // create random sphere
+  createSphere(Math.random() * 0.5, {
+    x: (Math.random() - 0.5) * 3,
+    y: 3,
+    z: (Math.random() - 0.5) * 3,
+  });
+};
+gui.add(debugObject, "createSphere");
 
 /**
  * Base
