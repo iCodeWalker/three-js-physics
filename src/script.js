e25f5ecc1dca415725581144543765c8979e55d3
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2024-03-09 00:29:54 +0530

    added : create box body for physics

--- src/script.js
+++ src/script.js
@@ -21,6 +21,22 @@ debugObject.createSphere = () => {
 };
 gui.add(debugObject, "createSphere");
 
+// #### ADDING TO DAT GUI ###
+
+debugObject.createBox = () => {
+  //   console.log("create box");
+  //createBox(0.5,0.5,0.5 { x: 0, y: 3, z: 0 });
+
+  // create random box
+  createBox(Math.random() * 0.5, Math.random() * 0.5, Math.random() * 0.5, {
+    x: (Math.random() - 0.5) * 3,
+    y: 3,
+    z: (Math.random() - 0.5) * 3,
+  });
+};
+
+gui.add(debugObject, "createBox");
+
 /**
  * Base
  */
@@ -243,6 +259,7 @@ const objectsToUpdate = [];
 // ##### Handling Multiple Objects ######
 // Create a createSphere function with "radius" and "position" parameters
 
+// ##################### SPHERE ###################
 const sphereGemometry = new THREE.SphereGeometry(1, 20, 20);
 const sphereMaterial = new THREE.MeshStandardMaterial({
   metalness: 0.3,
@@ -283,6 +300,48 @@ const createSphere = (radius, position) => {
 };
 
 createSphere(0.5, { x: 0, y: 3, z: 0 });
+
+// ##################### BOXES ###################
+const boxGemometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+});
+
+const createBox = (width, height, depth, position) => {
+  // ##### Three.js scene
+  // ##### Create a Mesh
+  const mesh = new THREE.Mesh(boxGemometry, boxMaterial);
+  // we can increase the width of sphere here
+  mesh.scale.set(width, height, depth);
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh);
+
+  // ##### Physics world
+  // ##### Create a Body
+
+  const shape = new CANNON.Box(
+    new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5)
+  );
+
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape: shape,
+    material: defaultMaterial,
+  });
+
+  body.position.copy(position);
+  world.addBody(body);
+
+  // push the mesh and body in the array
+  objectsToUpdate.push({
+    mesh: mesh,
+    body: body,
+  });
+};
 
 /**
  * Animate
