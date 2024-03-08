107b3d5ad7f1462563c75630800a2a640afbab76
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2024-03-09 00:39:36 +0530

    added : rotatation to objects when they fall

--- src/script.js
+++ src/script.js
@@ -375,6 +375,8 @@ const tick = () => {
 
   for (const object of objectsToUpdate) {
     object.mesh.position.copy(object.body.position);
+    // To rotate objects when they fall on each other
+    object.mesh.quaternion.copy(object.body.quaternion);
   }
 
   // Update controls
